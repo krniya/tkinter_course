bc2d84c2ec37244b13d415202daadb54d1de1abc
Nitish Kumar <krnitish@live.in>
2022-12-06 20:14:15 +0530

    Comments added for later refernce

--- FirstTkinter.py
+++ FirstTkinter.py
@@ -1,40 +1,38 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk # for native design
 
-def greet():
-    print(f"Hello, {user_name.get() or 'World'}!")
+root = tk.Tk() # root as main window
 
-root = tk.Tk()
-root.title("Greetings")
+root.title("Greetings") # title of window
 
-root.columnconfigure(0, weight=1)
+root.columnconfigure(0, weight=1) # assigning weight
 
-user_name = tk.StringVar()
+user_name = tk.StringVar() # username for input by user
 
-input_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
-input_frame.grid(row=0, column=0, sticky="EW")
+input_frame = ttk.Frame(root, padding=(20, 10, 20, 0)) # frame for input
+input_frame.grid(row=0, column=0, sticky="EW") # grid for positioning
 
-input_frame.columnconfigure(0,weight=1)
+input_frame.columnconfigure(0,weight=1) 
 input_frame.columnconfigure(1,weight=1)
 
-name_label = ttk.Label(input_frame, text="Greetings", padding=(30,10))
-name_label.grid(row=0, column=0, padx=(0,10), sticky="EW")
+name_label = ttk.Label(input_frame, text="Greetings", padding=(30,10)) # Text
+name_label.grid(row=0, column=0, padx=(0,10), sticky="EW")  # grid for positioning
 
-name_entry = ttk.Entry(input_frame,width=15, textvariable=user_name)
-name_entry.grid(row=0, column=1, sticky="EW")
-name_entry.focus()
+name_entry = ttk.Entry(input_frame,width=15, textvariable=user_name) # User input 
+name_entry.grid(row=0, column=1, sticky="EW") # grid for positioning
+name_entry.focus() # focus is for input selection as windows open
 
-button_frame = ttk.Frame(root, padding=(20, 10))
-button_frame.grid(sticky="EW", row=1, column=0)
+button_frame = ttk.Frame(root, padding=(20, 10)) # frame for button
+button_frame.grid(sticky="EW", row=1, column=0) # grid for positioning
 
-button_frame.columnconfigure(0,weight=1)
+button_frame.columnconfigure(0,weight=1) 
 button_frame.columnconfigure(1,weight=1)
 
 
-greet_btn = ttk.Button(button_frame, text="Greet", command=greet)
+greet_btn = ttk.Button(button_frame, text="Greet", command=greet) #button for greet, greet command is function name to be called
 greet_btn.grid(row=1, column=0, sticky="EW")
 
-quit_btn = ttk.Button(button_frame, text="Quit", command=root.destroy)
+quit_btn = ttk.Button(button_frame, text="Quit", command=root.destroy) #button for quit, root.destroy remove the screen
 quit_btn.grid(row=1, column=1, sticky="EW")
 
-root.mainloop()
+root.mainloop() # loops the screen and python event loops start
