9f4f2ae46dcc929be1ece4243da76ba63947c384
Nitish Kumar <krnitish@live.in>
2022-12-17 23:11:28 +0530

    Distance coverter finished - Added enter key binding - changed font - update padding

--- distanceConverter.py
+++ distanceConverter.py
@@ -1,5 +1,6 @@
-from calendar import c
+
 import tkinter as tk
+import tkinter.font as font
 from tkinter import ttk
 from turtle import st
 from windows import set_dpi_awareness
@@ -9,6 +10,9 @@ set_dpi_awareness() # dpi setting for windows
 #Basic screen setups
 root = tk.Tk()
 root.title("Distance Coverter")
+
+font.nametofont("TkDefaultFont").configure(size=15)
+
 root.columnconfigure(0, weight=1)
 
 main = ttk.Frame(root, padding=(30, 15))
@@ -30,16 +34,25 @@ def calculate_feet(*args):
 # Converter screeen
 
 meter_label = ttk.Label(main, text="Meters:")
-meter_input = ttk.Entry(main, width=10, textvariable=meters_values)
+meter_input = ttk.Entry(main, width=10, textvariable=meters_values, font=("Arial", 15))
 feet_label = ttk.Label(main, text="Feet:")
 feet_display = ttk.Label(main, text="Feet show here", textvariable=feet_value)
 calc_button = ttk.Button(main, text="Calculate", command=calculate_feet)
 
-meter_label.grid(column=0, row=0, sticky="W", padx=5, pady=5)
-meter_input.grid(column=1, row=0, sticky="EW", padx=5, pady=5)
+meter_label.grid(column=0, row=0, sticky="W")
+meter_input.grid(column=1, row=0, sticky="EW")
 meter_input.focus()
-feet_label.grid(column=0, row=1, sticky="W", padx=5, pady=5)
-feet_display.grid(column=1, row=1, sticky="EW", padx=5, pady=5)
-calc_button.grid(column=0, row=2, columnspan=2, sticky="EW", padx=5, pady=5)
+feet_label.grid(column=0, row=1, sticky="W")
+feet_display.grid(column=1, row=1, sticky="EW")
+calc_button.grid(column=0, row=2, columnspan=2, sticky="EW")
+
+# Updating padding
+for child in main.winfo_children():
+    child.grid_configure(padx=15, pady=15)
+
+# Adding key binding
+
+root.bind("<Return>", calculate_feet) 
+root.bind("<KP_Enter>", calculate_feet) 
 
 root.mainloop()
