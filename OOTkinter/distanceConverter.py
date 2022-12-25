fc199693717989f2cdc7aadad348b9e14d5e4747
Nitish Kumar <krnitish@live.in>
2022-12-25 23:01:08 +0530

    Object Oriented Tkinter - Added switch between frame functionality

--- OOTkinter/distanceConverter.py
+++ OOTkinter/distanceConverter.py
@@ -12,16 +12,26 @@ class DistanceConverter(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Distance Coverter")
+        self.frames = dict()
         container = ttk.Frame(self)
         container.grid(padx=60, pady=30, sticky="EW")
 
-        frame = FeetToMeter(container)
-        frame.grid(row=0, column=0, sticky="NSEW")
-        self.bind("<Return>", frame.calculate) 
-        self.bind("<KP_Enter>", frame.calculate) 
+        feetToMeterFrame = FeetToMeter(container, self)
+        feetToMeterFrame.grid(row=0, column=0, sticky="NSEW")
+
+        meterToFeetFrame = MeterToFeet(container, self)
+        meterToFeetFrame.grid(row=0, column=0, sticky="NSEW")
+
+        self.frames[FeetToMeter] = feetToMeterFrame
+        self.frames[MeterToFeet] = meterToFeetFrame
+        
+    def show_frame(self, container):
+        frame = self.frames[container]
+        frame.tkraise()
+
 
 class MeterToFeet(ttk.Frame):
-    def __init__(self, container, **kwargs):
+    def __init__(self, container, controller,  **kwargs):
         super().__init__(container, **kwargs)
         self.meters_values = tk.StringVar()
         self.feet_value = tk.StringVar()
@@ -29,7 +39,8 @@ class MeterToFeet(ttk.Frame):
         meter_input = ttk.Entry(self, width=10, textvariable=self.meters_values, font=("Arial", 15))
         feet_label = ttk.Label(self, text="Feet:")
         feet_display = ttk.Label(self, text="Feet show here", textvariable=self.feet_value)
-        calc_button = ttk.Button(self, text="Calculate", command=self.calculate_feet)
+        calc_button = ttk.Button(self, text="Calculate", command=self.calculate)
+        switch_page = ttk.Button(self, text="Swich FeetToMeter", command= lambda: controller.show_frame(FeetToMeter))
 
         meter_label.grid(column=0, row=0, sticky="W")
         meter_input.grid(column=1, row=0, sticky="EW")
@@ -37,6 +48,7 @@ class MeterToFeet(ttk.Frame):
         feet_label.grid(column=0, row=1, sticky="W")
         feet_display.grid(column=1, row=1, sticky="EW")
         calc_button.grid(column=0, row=2, columnspan=2, sticky="EW")
+        switch_page.grid(column=0, row=3, columnspan=2, sticky="EW")
 
         # Updating padding
         for child in self.winfo_children():
@@ -51,7 +63,7 @@ class MeterToFeet(ttk.Frame):
             pass
 
 class FeetToMeter(ttk.Frame):
-    def __init__(self, container, **kwargs):
+    def __init__(self, container, controller, **kwargs):
         super().__init__(container, **kwargs)
         self.meters_values = tk.StringVar()
         self.feet_value = tk.StringVar()
@@ -60,13 +72,14 @@ class FeetToMeter(ttk.Frame):
         meter_label = ttk.Label(self, text="Meter:")
         meter_display = ttk.Label(self, text="meter show here", textvariable=self.meters_values)
         calc_button = ttk.Button(self, text="Calculate", command=self.calculate)
-
+        switch_page = ttk.Button(self, text="Swich MeterToFeet", command= lambda: controller.show_frame(MeterToFeet))
 
         feet_label.grid(column=0, row=0, sticky="W")
         feet_input.grid(column=1, row=0, sticky="EW")
         feet_input.focus()
         meter_label.grid(column=0, row=1, sticky="W")
         meter_display.grid(column=1, row=1, sticky="EW")
+        switch_page.grid(column=0, row=3, columnspan=2, sticky="EW")
         
         calc_button.grid(column=0, row=2, columnspan=2, sticky="EW")
         # Updating padding
